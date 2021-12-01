979de6af322617904af877d50fabb45f7ce497b5
unknown <liangty1998@hotmail.com>
2021-11-30 20:38:56 -0800

    precondition

--- algorithm.jl
+++ algorithm.jl
@@ -1,36 +1,296 @@
 
-
+overall = 0
+iter_overall = 0
 # return U and M such that A ~= U^TM, k is the rank, lambda is the regularizing constant
 @everywhere function compute_factor(A_row, A_col, k::Int64, lambda::Float64, max_iter::Int64)
     U = zeros(k, length(A_row))
     M = zeros(k, length(A_col))
-
+    sampling_factor = 2.5
     # initialize M by assigning average movie of that movie as first row
 
     for i = 1 : length(A_col)
         M[1, i] = mean(A_col[i].value)
     end
-    M[2 : end, :] = rand(k - 1, size(M, 2))
+    M[2 : end, :] = rand(Normal(0, 2), k - 1, size(M, 2))
+    
+
+    
+    # find dimension of placeholder
+    max_len = 0
+    for i = 1 : size(U, 2)
+        max_len = max(length(A_row[i].index), max_len)
+    end
+    for i = 1 : size(M, 2)
+        max_len = max(length(A_col[i].index), max_len)
+    end
+    max_len = max(max_len, k)
+
+    # placeholder1 stores the sketch matrix
+    placeholder1 = zeros(Int64(ceil(sampling_factor * max_len)) + 1, k + 1)
+    # placeholder 2 stores the concatenated matrix of the new system
+    placeholder2 = zeros(max_len + k + 1, k + 1)
 
     for iter = 1 : max_iter
+        println("iter: ", iter)
         # fix M, optimize over U
-        for i = 1 : size(U, 2)
+        @time Threads.@threads for i = 1 : size(U, 2)
+        #for i = 1 : 10
             user_movie = A_row[i].index
             M_sub = M[:, user_movie]
             #U[:, i] = (M_sub * M_sub' + lambda * I) \ (M_sub * A_row[i].value)
-            U[:, i] = lsqr(M_sub', A_row[i].value; damp=sqrt(lambda), atol=0)
-
+            t1 = time()
+            #@time x, ch = IterativeSolvers.lsqr(M_sub', A_row[i].value; damp=sqrt(lambda), atol=0, log=true)
+            U[:, i], stats = Krylov.lsqr(M_sub', A_row[i].value, λ=sqrt(lambda))
+            #println("no pre cond: ", cond(M_sub'))
+            #println("no pre time: ", time() - t1)
+            #println(ch)
+            #@time U[:, i] = solve_with_preconditioner(M_sub', A_row[i].value, sampling_factor, lambda, placeholder1, placeholder2)
+            #U[:, i] = solve_with_preconditioner(M_sub', A_row[i].value, sampling_factor, lambda)
+            if mod(i, 1000) == 0
+                println("no pre cond: ", cond(M_sub'))
+            end
         end
-
+        println("qr overall: ", overall)
+        println("iter overall: ", iter_overall)
         # fix U, optimize over M
-        for i = 1 : size(M, 2)
+        
+        #@time Threads.@threads for i = 1 : size(M, 2)
+        for i = 1 : 10
             movie_user = A_col[i].index
             U_sub = U[:, movie_user]
             #M[:, i] = (U_sub * U_sub' + lambda * I) \ (U_sub * A_col[i].value)
-            M[:, i] = lsqr(U_sub', A_col[i].value; damp=sqrt(lambda), atol=1e-12)
+            #M[:, i] = IterativeSolvers.lsqr(U_sub', A_col[i].value; damp=sqrt(lambda), atol=0)
+            #M[:, i] = solve_with_preconditioner(U_sub', A_col[i].value, sampling_factor, lambda, placeholder1, placeholder2)
+            M[:, i] = solve_with_preconditioner(U_sub', A_col[i].value, sampling_factor, lambda)
+            @time M[:, i], stats = Krylov.lsqr(U_sub', A_col[i].value, λ=sqrt(lambda), history=true)
+            println("non preconditioned iter: ", length(stats.residuals))
+            println("no pre cond: ", cond([U_sub'; sqrt(lambda) * I]))
+            if mod(i, 1000) == 0
+                println("no pre cond: ", cond([U_sub'; sqrt(lambda) * I]))
+            end
         end
+        
     end
 
     return U', M
 end
+
+
+why = []
+other=[]
+@everywhere function solve_with_preconditioner(A,  b::Vector, sampling_factor, lambda::Float64, placeholder1, placeholder2)
+    m, n = size(A)
+    if m < n
+        A = A'
+    end
+    height = Int64(ceil(min(n, m) * sampling_factor))
+    #S = rand(Normal(), height, max(n, m) + min(m, n))
+    S = SRFT(height)
+
+    # svd method
+    
+    #=
+    @time svd_object = svd([S * A; sqrt(lambda) * I])
+    cutoff = findall(x -> x < 1e-9, svd_object.S)
+    C = []
+    if isempty(cutoff)
+        C = svd_object.V * Diagonal(inv.(svd_object.S))
+    else
+        C = svd_object.V[:, 1 : cutoff[1]] * Diagonal(inv.(svd_object.S[1 : cutoff]))
+    end
+    =#
+    
+    # qr method
+    
+
+    # setup placeholder 2
+    placeholder2[1 : max(m, n), 1 : min(m, n)] .= A
+    placeholder2[1 : m, min(m, n) + 1] .= b
+    for r = max(m, n) + 1 : max(m, n) + min(m, n)
+        comp = r - max(m, n)
+        for c = 1 : min(m, n)
+            if comp != c
+                placeholder2[r, c] = 0
+            else
+                placeholder2[r, c] = sqrt(lambda)
+            end
+        end
+    end
+    
+    # set up placeholder 1
+    t1 = time()
+    @time @views mul!(placeholder1[1 : height, 1 : min(n, m) + 1], S, placeholder2[1 : max(m, n) + min(m, n), 1 : min(m, n) + 1])
+    #@views mul!(placeholder1[1 : height, 1 : min(n, m)], S, A)
+    #=
+    for r = height + 1 : height + min(n, m)
+        comp = r - height
+        for c = 1 : min(n, m)
+            if comp != c
+                placeholder1[r, c] = 0
+            else
+                placeholder1[r, c] = sqrt(lambda)
+            end
+        end
+    end
+    =#
+    
+    global overall += (time() - t1)
+
+    
+    
+    @time @views Q, R = qr(placeholder1[1 : height, 1 : min(n, m)])
+    #~, R = qr([S * A; sqrt(lambda) * I])
+    
+    
+    #println("qr time: ", time() - t1)
+    println(size(A))
+    
+
+
+    
+    #println("prev_error: ", norm(placeholder2[1 : max(m, n) + min(m, n), 1 : min(m, n)] - [A; sqrt(lambda) * I]))
+    @time @views BLAS.trsm!('R', 'U', 'N', 'N', 1.0, R, placeholder2[1 : max(m, n) + min(m, n), 1 : min(m, n)])
+    @views new_system = placeholder2[1 : max(m, n) + min(m, n), 1 : min(m, n)]
+    initial = placeholder1[1 : height, min(m, n) + 1]
+    @time initial = Matrix(Q)' * initial
+
+
+    #new_system = (R' \ [A; sqrt(lambda) * I]')'
+    #println("without pre cond: ", cond([A; sqrt(lambda) * I]))
+    #println("with pre cond: ", cond(new_system))
+    t2 = time()
+    if m >= n
+        #z = lsqr((R' \ [A; sqrt(lambda) * I]')', [b; zeros(n)])
+        #z, ch = lsqr(new_system, [b; zeros(n)]; log=true, verbose=true)
+        @time lsqr!(initial, new_system, [b; zeros(n)])
+        #gg, ch1 = lsqr(A, b; damp=sqrt(lambda), log=true, verbose=true)
+        #lsqr(A, rand(m); damp=sqrt(lambda), log=true, verbose=true)
+        #println(norm(A * (R \ initial) - b) + norm(R \ initial))
+        #println(norm(A * gg - b) + norm(gg))
+        #println(cond([A; sqrt(lambda) * I]))
+        #println(cond(new_system))
+        global iter_overall += (time() - t2)
+        return R \ initial
+    else
+        #@time z = lsqr((A * C)', C' * b * sqrt(lambda))
+        
+        
+        #z, ch = lsqr(new_system, R' \ (b * sqrt(lambda)); log=true)
+        #println(ch)
+        #trsv!(ul, tA, dA, A, b)
+        rightside = BLAS.trsv('U', 'T', 'N', R, (b * sqrt(lambda)))
+        z, ch = lsmr(new_system', rightside; log=true)
+        #global iter_overall += (time() - t2)
+        return z[1 : n] / sqrt(lambda)
+    end
+    
+end
+
+
+@everywhere function solve_with_preconditioner(A,  b::Vector, sampling_factor, lambda::Float64)
+    m, n = size(A)
+    if m < n
+        A = A'
+    end
+    height = Int64(ceil(min(n, m) * sampling_factor))
+    #S = rand(Normal(), height, max(n, m) + min(m, n))
+    #S = rand(Normal(), height, max(n, m))
+    S = SRFT(height)
+
+    t1 = time()
+    extended = [A; sqrt(lambda) * I]
+    A_hat = S * extended
+    b_hat = S * reshape(b, (length(b), 1))
+    b_hat = b_hat[:]
+    global overall += (time() - t1)
+
+    println("t1: ", time() - t1)
+    println("size: ", size(A))
+    
+    #@time Q, R = qr([A_hat; sqrt(lambda) * I])
+    Q, R = qr(A_hat)
+    #~, R = qr([S * A; sqrt(lambda) * I])
+    
+    
+    #println("qr time: ", time() - t1)
+    # println(size(A))
+    # println(size(Q))
+    # println(size(b_hat))
+
+
+    
+    #println("prev_error: ", norm(placeholder2[1 : max(m, n) + min(m, n), 1 : min(m, n)] - [A; sqrt(lambda) * I]))
+
+    BLAS.trsm!('R', 'U', 'N', 'N', 1.0, R, extended)
+    new_system = extended
+    #initial = Matrix(Q)'[:, 1 : height] * b_hat
+    #initial = zeros(n)
+
+    #new_system = (R' \ [A; sqrt(lambda) * I]')'
+    #println("without pre cond: ", cond([A; sqrt(lambda) * I]))
+    #println("with pre cond: ", cond(new_system))
+    t2 = time()
+    if m >= n
+        #z = lsqr((R' \ [A; sqrt(lambda) * I]')', [b; zeros(n)])
+        #z, ch = lsqr(new_system, [b; zeros(n)]; log=true, verbose=true)
+        #IterativeSolvers.lsqr!(initial, new_system, [b; zeros(n)]; log=true, verbose=true)
+        #gg, ch1 = lsqr(A, b; damp=sqrt(lambda), log=true, verbose=true)
+        #lsqr(A, rand(m); damp=sqrt(lambda), log=true, verbose=true)
+        @time initial, info = Krylov.lsqr(new_system, [b; zeros(n)], history=true)
+        println("preconditioned num iter: ", length(info.residuals))
+        #println(norm(A * (R \ initial) - b) + norm(R \ initial))
+        #println(norm(A * gg - b) + norm(gg))
+        #println(cond([A; sqrt(lambda) * I]))
+        println("precond cond number: ", cond(new_system))
+        global iter_overall += (time() - t2)
+        return R \ initial
+    else
+        #@time z = lsqr((A * C)', C' * b * sqrt(lambda))
+        
+        
+        #z, ch = lsqr(new_system, R' \ (b * sqrt(lambda)); log=true)
+        #println(ch)
+        #trsv!(ul, tA, dA, A, b)
+        rightside = BLAS.trsv('U', 'T', 'N', R, (b * sqrt(lambda)))
+        #z, ch = IterativeSolvers.lsmr(new_system', rightside; log=true)
+        z, stat = Krylov.lsmr(new_system', rightside)
+        #global iter_overall += (time() - t2)
+        return z[1 : n] / sqrt(lambda)
+    end
+    
+end
+
+
+
+@everywhere function compute_global_preconditioner(A, UM, total_len, sampling_factor, lambda::Float64)
+    count = 0
+    for i = 1 : total_len
+        count += length(A[i].index)
+    end
+
+    all_indices = zeros(Int64, count)
+    current = 1
+    for i = 1 : total_len
+        cur_indices = A[i].index
+        all_indices[current : current + length(cur_indices) - 1] .= cur_indices
+        current += length(cur_indices)
+    end
+
+    O = UM[:, all_indices]
+    m, n = size(O)
+    if m < n
+        O = O'
+    end
+    S = rand(Normal(), min(n, m) * sampling_factor, max(n, m))
+    @time Q, R = qr([S * O; sqrt(lambda) * I])
+    println("size R: ", size(R))
+    return R
+
+end
+
+
+
+
+
+
 
--- read.jl
+++ read.jl
@@ -6,6 +6,10 @@ using Distributed
 @everywhere using Distributions
 @everywhere using LinearAlgebra
 @everywhere using IterativeSolvers
+@everywhere using FFTW
+@everywhere using LowRankApprox
+@everywhere using Krylov
+# @everywhere using Krylov
 include("algorithm.jl")
 
 @everywhere struct sparse_data_node{T1<:Union{Array{Float64, 1}, Array{Int64, 1}}, 
@@ -23,42 +27,135 @@ end
 #     id_mapping[movieId[i]] = i
 # end
 
-# read in data
-rating_csv = CSV.File(open("ml-25m/ratings.csv"); limit=20000, select=[1,2,3])
-df = DataFrame(rating_csv)
-
-# stored in compressed column format
-gd_col = groupby(df, :movieId; sort=true)
-sparse_data_matrix_column = Array{sparse_data_node}(undef, length(gd_col))
-for i = 1 : length(gd_col)
-    sparse_data_matrix_column[i] = sparse_data_node(copy(gd_col[i].rating), copy(gd_col[i].userId), i)
-end
-
-gd_col_keys = keys(gd_col)
-id_mapping = zeros(Int64, gd_col_keys[end].movieId)
-for i = 1 : length(gd_col)
-    id_mapping[gd_col_keys[i].movieId] = i
-end
-
-# stored in compressed row format
-gd = groupby(df, :userId)
-sparse_data_matrix_row = Array{sparse_data_node}(undef, length(gd))
-for i = 1 : length(gd)
-    sparse_data_matrix_row[i] = sparse_data_node(copy(gd[i].rating), copy(id_mapping[gd[i].movieId]), i)
-end
-
-
-iter = [1, 5, 10, 50]
-for i in iter
-    @time Ut, M = compute_factor(sparse_data_matrix_row, sparse_data_matrix_column, 100, 0.1, i)
-
-    # verify
-    actual = zeros(length(gd), length(gd_col))
-    for i = 1 : length(gd)
-        actual[i, id_mapping[gd[i].movieId]] = gd[i].rating
+function gooby()
+    # read in data
+    rating_csv = CSV.File(open("ml-25m/ratings.csv"); limit=2000000, select=[1,2,3])
+    df = DataFrame(rating_csv)
+    
+    # stored in compressed column format
+    gd_col = groupby(df, :movieId; sort=true)
+    sparse_data_matrix_column = Array{sparse_data_node}(undef, length(gd_col))
+    for i = 1 : length(gd_col)
+        sparse_data_matrix_column[i] = sparse_data_node(copy(gd_col[i].rating), copy(gd_col[i].userId), i)
     end
-    zero_index = findall(x -> x == 0, actual)
-    calculated = Ut * M
-    calculated[zero_index] .= 0
-    println(norm(calculated - actual) / norm(actual))
+
+    gd_col_keys = keys(gd_col)
+    id_mapping = zeros(Int64, gd_col_keys[end].movieId)
+    for i = 1 : length(gd_col)
+        id_mapping[gd_col_keys[i].movieId] = i
+    end
+
+    # stored in compressed row format
+    gd = groupby(df, :userId)
+    sparse_data_matrix_row = Array{sparse_data_node}(undef, length(gd))
+    for i = 1 : length(gd)
+        sparse_data_matrix_row[i] = sparse_data_node(copy(gd[i].rating), copy(id_mapping[gd[i].movieId]), i)
+    end
+
+
+    #iter = [1, 5, 10, 50]
+    iter = [5]
+    rank_k = 100
+    regularization = 0.01
+    for i in iter
+        @time Ut, M = compute_factor(sparse_data_matrix_row, sparse_data_matrix_column, rank_k, regularization, i)
+
+        # verify
+        actual = zeros(length(gd), length(gd_col))
+        for i = 1 : length(gd)
+            actual[i, id_mapping[gd[i].movieId]] = gd[i].rating
+        end
+        zero_index = findall(x -> x == 0, actual)
+        calculated = Ut * M
+        calculated[zero_index] .= 0
+        println(norm(calculated - actual) / norm(actual))
+        println(size(actual))
+    end
+    
+
+end
+
+function dolan()
+    m = 5000
+    n = 6000
+    k = 50
+    observe_rate = 0.5
+    regularization = 0.00001
+    generate_U = rand(m, k) .* sqrt(5 / k)
+    generate_M = rand(n, k) .* sqrt(5 / k)
+    generator = Bernoulli(observe_rate)
+    all_indices = CartesianIndex[]
+
+    # compressed row scheme
+    sparse_data_matrix_row = Array{sparse_data_node}(undef, m)
+    element_mapping = Vector{Dict{Int64, Int64}}(undef, m)
+    for i = eachindex(element_mapping)
+        element_mapping[i] = Dict{Int64, Int64}()
+    end
+    for r = 1 : m
+        count = 1
+        for c = 1 : n
+            if rand(generator) != 0
+                push!(all_indices, CartesianIndex(r, c))
+                get!(element_mapping[r], c, count)
+                count += 1
+            end
+        end
+    end
+    for i = 1 : m 
+        sparse_data_matrix_row[i] = sparse_data_node(zeros(Float64, length(element_mapping[i])), zeros(Int64, length(element_mapping[i])), i)
+    end
+    max_val = 0
+    min_val = 5
+    weight = [2,4,6,15,17]
+    @time Threads.@threads for i in eachindex(all_indices)
+        cur_index = all_indices[i]
+        row = cur_index[1]
+        col = cur_index[2]
+        map_idx = get(element_mapping[row], col, -1)
+        @assert map_idx != -1
+        @views val = dot(generate_U[row, :], generate_M[col, :])
+        #val = searchsortedfirst(weight, rand() * 17)
+        sparse_data_matrix_row[row].value[map_idx] = val
+        max_val = max(val, max_val)
+        min_val = min(val, min_val)
+        sparse_data_matrix_row[row].index[map_idx] = col
+    end
+    println("max_val: ", max_val)
+    println("min_val: ", min_val)
+    # compressed column scheme
+    sparse_data_matrix_column = Array{sparse_data_node}(undef, n)
+    for i = 1 : n
+        sparse_data_matrix_column[i] = sparse_data_node(zeros(Float64, 0), zeros(Int64, 0), i)
+    end
+    for r = 1 : m
+        cur_row = sparse_data_matrix_row[r]
+        cur_index = cur_row.index
+        cur_value = cur_row.value 
+        for c = eachindex(cur_index)
+            push!(sparse_data_matrix_column[cur_index[c]].index, r)
+            push!(sparse_data_matrix_column[cur_index[c]].value, cur_value[c])
+        end
+    end
+
+    
+    iter = [1]
+    rank_k = 1500
+    for i in iter
+        @time Ut, M = compute_factor(sparse_data_matrix_row, sparse_data_matrix_column, rank_k, regularization, i)
+
+        # verify
+        #=
+        actual = zeros(length(gd), length(gd_col))
+        for i = 1 : length(gd)
+            actual[i, id_mapping[gd[i].movieId]] = gd[i].rating
+        end
+        zero_index = findall(x -> x == 0, actual)
+        calculated = Ut * M
+        calculated[zero_index] .= 0
+        println(norm(calculated - actual) / norm(actual))
+        println(size(actual))
+        =#
+    end
+    
 end
